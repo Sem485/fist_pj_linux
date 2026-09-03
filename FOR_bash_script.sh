19d10b1c39dc9950837e0574e5497d8b3325fc0e
Sem485 <stepanishevsem@gmail.com>
2026-09-03 17:15:08 +0300

    Create FOR_bash_script.sh
    цикл FOR

---
+++ FOR_bash_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#for цикл как работает
+echo "Starting check all servers..."
+echo "............................."
+servers=("bd-server" "web-server" "cache-server" "public-server")
+for server in "${servers[@]}"; do #перебор заданных серверов, цикт в этом
+echo "connecting to servers..."
+sleep 3 #для имитации проверки
+echo "Connect complete"
+echo ""
+done
+echo "All server working"
